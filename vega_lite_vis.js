8d6f61ee8927facbf0d0c6fd2f6762ab24407ce1
nhun0005 <142361107+nhun0005@users.noreply.github.com>
2023-09-23 20:56:59 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var mapVis = "animated_map.vg.json";
+vegaEmbed("#mapVis", mapVis).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
